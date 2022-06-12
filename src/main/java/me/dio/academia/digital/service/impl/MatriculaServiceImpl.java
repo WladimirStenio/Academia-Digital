278f70f7cdb0b9ce0d09cd32dd7de61ccbe09486
WladimirStenio <wladimirstenio75@gmail.com>
2022-06-11 22:58:36 -0300

    first commit

---
+++ src/main/java/me/dio/academia/digital/service/impl/MatriculaServiceImpl.java
@@ -0,0 +1,43 @@
+package me.dio.academia.digital.service.impl;
+
+
+import me.dio.academia.digital.entity.Aluno;
+import me.dio.academia.digital.entity.Matricula;
+import me.dio.academia.digital.entity.form.MatriculaForm;
+import me.dio.academia.digital.repository.AlunoRepository;
+import me.dio.academia.digital.repository.AvaliacaoFisicaRepository;
+import me.dio.academia.digital.repository.MatriculaRepository;
+import me.dio.academia.digital.service.IMatriculaService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class MatriculaServiceImpl implements IMatriculaService {
+
+
+    @Autowired
+    private MatriculaRepository matriculaRepository;
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    @Override
+    public Matricula create(MatriculaForm form) {
+        return null;
+    }
+
+    @Override
+    public Matricula get(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Matricula> getAll() {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
